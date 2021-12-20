18ab84e24acc780970996a585d1256dc0cb5b4d1
unknown <bileljabrane6@gmail.com>
2021-12-20 14:18:43 +0100

    update

--- src/App.js
+++ src/App.js
@@ -11,20 +11,34 @@ class App extends React.Component {
         imgsrc: { pic },
         profession: "Web developer"
       },
-      b: false
+      b: false,
+      count : 0
     }
   }
+
+  componentDidMount(){
+
+    setInterval(()=> {
+      this.setState({ count : this.state.count+1})
+    }, 1000)
+    // const Counter=()=>{ this.setState({ count : <div>{this.state.count+1, 1000}</div>})}
+    
+  }
+ 
+
+
+
   render() {
     return (
       <div>
         <button onClick={()=>this.setState({ b: !this.state.b })}>Hide/Show</button>
-
         {
           this.state.b ? "" :
             <div>
               <h1>{this.state.Person.fullName}</h1>
               <h3>{this.state.Person.bio}</h3>
               <h2>{this.state.Person.profession}</h2>
+              <h1>{this.state.count}</h1>
               <img height='300px' width='200' src={pic}></img>
             </div>
 
